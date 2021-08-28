d25f5b3740f3b469e359c50a46efb42e60f360ef
Anubhav Kalra <anubhavkalra2001@gmail.com>
2021-08-29 02:33:27 +0530

    Added web application files

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def final(f,g):
                 description=label.description
             ), ignore_index=True
         )
-    df2 = pd.DataFrame(columns=['best_guess_labels', 'web_entities'])
+    df2 = pd.DataFrame(columns=['best_guess_labels'])
     if annotations.best_guess_labels:
         for annotation in annotations.best_guess_labels:
             df2 = df2.append(
@@ -71,9 +71,10 @@ def final(f,g):
                 best_guess_labels = annotation.label
                 ), ignore_index=True
             )
+    df5 = pd.DataFrame(columns=['web_entities'])
     if annotations.web_entities:
         for entity in annotations.web_entities:
-            df2 = df2.append(
+            df5 = df5.append(
                 dict(
                 web_entities = entity.description
                 ), ignore_index=True
@@ -96,24 +97,39 @@ def final(f,g):
                 text_detection = text.description
                 ), ignore_index=True
             )
-    df1 = df1.reset_index(drop=True)
-    df2 = df2.reset_index(drop=True)
-    df1.join(df2)
-    df1 = df1.reset_index(drop=True)
-    df3 = df3.reset_index(drop=True)
-    df1.join(df3)
-    df1 = df1.reset_index(drop=True)
-    df4 = df4.reset_index(drop=True)
-    df1.join(df4)
+    # df1 = df1.reset_index(drop=True)
+    # df2 = df2.reset_index(drop=True)
+    # df1.join(df2)
+    # df1 = df1.reset_index(drop=True)
+    # df3 = df3.reset_index(drop=True)
+    # df1.join(df3)
+    # df1 = df1.reset_index(drop=True)
+    # df4 = df4.reset_index(drop=True)
+    # df1.join(df4)
     # output_file_1="output/result1.txt"
     # output_file_2="output/result2.txt"
     # output_file_3="output/result3.txt"
     # output_file_4="output/result4.txt"
-    # df1 = df1.replace(np.nan, '', regex=True)
-    # df2 = df2.replace(np.nan, '', regex=True)
-    # df3 = df3.replace(np.nan, '', regex=True)
-    # df4 = df4.replace(np.nan, '', regex=True)
-    return(df1)
+    df1 = df1.replace(np.nan, '', regex=True)
+    df2 = df2.replace(np.nan, '', regex=True)
+    df3 = df3.replace(np.nan, '', regex=True)
+    df4 = df4.replace(np.nan, '', regex=True)
+    df5 = df5.replace(np.nan, '', regex=True)
+    print(df1)
+    print(df2)
+    print(df5)
+    print(df3)
+    print(df4)
+    stack1 = pd.concat([df1, df2], axis = 1, sort = False)
+    stack2 = pd.concat([stack1, df5], axis = 1, sort = False)
+    stack3 = pd.concat([stack2, df3], axis = 1, sort = False)
+    stack4 = pd.concat([stack3, df4], axis = 1, sort = False)
+    stack4 = stack4.replace(np.nan, '', regex=True)
+    print(stack4)
+
+    return render_template("results.html", column_names=stack4.columns.values, row_data=list(stack4.values.tolist()), zip=zip)
+
+
     # with open(output_file_1, 'w', encoding="utf-8") as l1:
     #     dfAsString = df1.to_string(header=False, index=False)
     #     l1.write(dfAsString)
